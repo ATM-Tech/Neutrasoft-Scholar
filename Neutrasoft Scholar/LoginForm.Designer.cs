e9b53e03b75cd4a6361caf3d6dc6f1cda0235ab7
Aiden Madaffri <Aiden.Madaffri@gmail.com>
2018-07-04 08:47:40 -0500

    Control names changed for better readabillity. Title bar text was also changed upon launch of the program

--- Neutrasoft Scholar/LoginForm.Designer.cs
+++ Neutrasoft Scholar/LoginForm.Designer.cs
@@ -28,138 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.scholarImageBox = new System.Windows.Forms.PictureBox();
-            this.usernameLabel = new System.Windows.Forms.Label();
-            this.passcodeLabel = new System.Windows.Forms.Label();
-            this.usernameTextBox = new System.Windows.Forms.TextBox();
-            this.passcodeTextBox = new System.Windows.Forms.TextBox();
-            this.errorLabel = new System.Windows.Forms.Label();
-            this.loginButton = new System.Windows.Forms.Button();
-            this.roleRadioButtonGroupBox = new System.Windows.Forms.GroupBox();
-            this.teacherRadioButton = new System.Windows.Forms.RadioButton();
-            this.studentRadioButton = new System.Windows.Forms.RadioButton();
-            ((System.ComponentModel.ISupportInitialize)(this.scholarImageBox)).BeginInit();
-            this.roleRadioButtonGroupBox.SuspendLayout();
+            this.imgScholar = new System.Windows.Forms.PictureBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPasscode = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPasscode = new System.Windows.Forms.TextBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.gpRole = new System.Windows.Forms.GroupBox();
+            this.rdoStudent = new System.Windows.Forms.RadioButton();
+            this.rdoTeacher = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.imgScholar)).BeginInit();
+            this.gpRole.SuspendLayout();
             this.SuspendLayout();
             // 
-            // scholarImageBox
+            // imgScholar
             // 
-            this.scholarImageBox.BackgroundImage = global::Neutrasoft_Scholar.Properties.Resources.Neutrasoft_Scholar_Image;
-            this.scholarImageBox.Location = new System.Drawing.Point(128, 12);
-            this.scholarImageBox.Name = "scholarImageBox";
-            this.scholarImageBox.Size = new System.Drawing.Size(192, 192);
-            this.scholarImageBox.TabIndex = 0;
-            this.scholarImageBox.TabStop = false;
+            this.imgScholar.BackgroundImage = global::Neutrasoft_Scholar.Properties.Resources.Neutrasoft_Scholar_Image;
+            this.imgScholar.Location = new System.Drawing.Point(128, 12);
+            this.imgScholar.Name = "imgScholar";
+            this.imgScholar.Size = new System.Drawing.Size(192, 192);
+            this.imgScholar.TabIndex = 0;
+            this.imgScholar.TabStop = false;
             // 
-            // usernameLabel
+            // lblUsername
             // 
-            this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameLabel.Location = new System.Drawing.Point(46, 343);
-            this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(102, 24);
-            this.usernameLabel.TabIndex = 1;
-            this.usernameLabel.Text = "Username:";
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(46, 343);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(102, 24);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Username:";
             // 
-            // passcodeLabel
+            // lblPasscode
             // 
-            this.passcodeLabel.AutoSize = true;
-            this.passcodeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passcodeLabel.Location = new System.Drawing.Point(50, 398);
-            this.passcodeLabel.Name = "passcodeLabel";
-            this.passcodeLabel.Size = new System.Drawing.Size(98, 24);
-            this.passcodeLabel.TabIndex = 2;
-            this.passcodeLabel.Text = "Passcode:";
+            this.lblPasscode.AutoSize = true;
+            this.lblPasscode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPasscode.Location = new System.Drawing.Point(50, 398);
+            this.lblPasscode.Name = "lblPasscode";
+            this.lblPasscode.Size = new System.Drawing.Size(98, 24);
+            this.lblPasscode.TabIndex = 2;
+            this.lblPasscode.Text = "Passcode:";
             // 
-            // usernameTextBox
+            // tbUsername
             // 
-            this.usernameTextBox.Location = new System.Drawing.Point(154, 348);
-            this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(261, 20);
-            this.usernameTextBox.TabIndex = 3;
+            this.tbUsername.Location = new System.Drawing.Point(154, 348);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(261, 20);
+            this.tbUsername.TabIndex = 3;
             // 
-            // passcodeTextBox
+            // tbPasscode
             // 
-            this.passcodeTextBox.Location = new System.Drawing.Point(154, 403);
-            this.passcodeTextBox.Name = "passcodeTextBox";
-            this.passcodeTextBox.PasswordChar = '*';
-            this.passcodeTextBox.Size = new System.Drawing.Size(261, 20);
-            this.passcodeTextBox.TabIndex = 4;
+            this.tbPasscode.Location = new System.Drawing.Point(154, 403);
+            this.tbPasscode.Name = "tbPasscode";
+            this.tbPasscode.PasswordChar = '*';
+            this.tbPasscode.Size = new System.Drawing.Size(261, 20);
+            this.tbPasscode.TabIndex = 4;
             // 
-            // errorLabel
+            // lblError
             // 
-            this.errorLabel.AutoSize = true;
-            this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.errorLabel.ForeColor = System.Drawing.Color.Red;
-            this.errorLabel.Location = new System.Drawing.Point(12, 252);
-            this.errorLabel.Name = "errorLabel";
-            this.errorLabel.Size = new System.Drawing.Size(0, 29);
-            this.errorLabel.TabIndex = 5;
-            this.errorLabel.Visible = false;
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(12, 252);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 29);
+            this.lblError.TabIndex = 5;
+            this.lblError.Visible = false;
             // 
-            // loginButton
+            // btnLogin
             // 
-            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginButton.Location = new System.Drawing.Point(50, 512);
-            this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(365, 57);
-            this.loginButton.TabIndex = 6;
-            this.loginButton.Text = "Login";
-            this.loginButton.UseVisualStyleBackColor = true;
-            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(50, 512);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(365, 57);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.loginButton_Click);
             // 
-            // roleRadioButtonGroupBox
+            // gpRole
             // 
-            this.roleRadioButtonGroupBox.Controls.Add(this.studentRadioButton);
-            this.roleRadioButtonGroupBox.Controls.Add(this.teacherRadioButton);
-            this.roleRadioButtonGroupBox.Location = new System.Drawing.Point(50, 441);
-            this.roleRadioButtonGroupBox.Name = "roleRadioButtonGroupBox";
-            this.roleRadioButtonGroupBox.Size = new System.Drawing.Size(365, 53);
-            this.roleRadioButtonGroupBox.TabIndex = 7;
-            this.roleRadioButtonGroupBox.TabStop = false;
+            this.gpRole.Controls.Add(this.rdoStudent);
+            this.gpRole.Controls.Add(this.rdoTeacher);
+            this.gpRole.Location = new System.Drawing.Point(50, 441);
+            this.gpRole.Name = "gpRole";
+            this.gpRole.Size = new System.Drawing.Size(365, 53);
+            this.gpRole.TabIndex = 7;
+            this.gpRole.TabStop = false;
             // 
-            // teacherRadioButton
+            // rdoStudent
             // 
-            this.teacherRadioButton.AutoSize = true;
-            this.teacherRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.teacherRadioButton.Location = new System.Drawing.Point(31, 19);
-            this.teacherRadioButton.Name = "teacherRadioButton";
-            this.teacherRadioButton.Size = new System.Drawing.Size(99, 28);
-            this.teacherRadioButton.TabIndex = 0;
-            this.teacherRadioButton.TabStop = true;
-            this.teacherRadioButton.Text = "Teacher";
-            this.teacherRadioButton.UseVisualStyleBackColor = true;
+            this.rdoStudent.AutoSize = true;
+            this.rdoStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoStudent.Location = new System.Drawing.Point(202, 19);
+            this.rdoStudent.Name = "rdoStudent";
+            this.rdoStudent.Size = new System.Drawing.Size(92, 28);
+            this.rdoStudent.TabIndex = 1;
+            this.rdoStudent.TabStop = true;
+            this.rdoStudent.Text = "Student";
+            this.rdoStudent.UseVisualStyleBackColor = true;
             // 
-            // studentRadioButton
+            // rdoTeacher
             // 
-            this.studentRadioButton.AutoSize = true;
-            this.studentRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.studentRadioButton.Location = new System.Drawing.Point(202, 19);
-            this.studentRadioButton.Name = "studentRadioButton";
-            this.studentRadioButton.Size = new System.Drawing.Size(92, 28);
-            this.studentRadioButton.TabIndex = 1;
-            this.studentRadioButton.TabStop = true;
-            this.studentRadioButton.Text = "Student";
-            this.studentRadioButton.UseVisualStyleBackColor = true;
+            this.rdoTeacher.AutoSize = true;
+            this.rdoTeacher.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoTeacher.Location = new System.Drawing.Point(31, 19);
+            this.rdoTeacher.Name = "rdoTeacher";
+            this.rdoTeacher.Size = new System.Drawing.Size(99, 28);
+            this.rdoTeacher.TabIndex = 0;
+            this.rdoTeacher.TabStop = true;
+            this.rdoTeacher.Text = "Teacher";
+            this.rdoTeacher.UseVisualStyleBackColor = true;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(471, 619);
-            this.Controls.Add(this.roleRadioButtonGroupBox);
-            this.Controls.Add(this.loginButton);
-            this.Controls.Add(this.errorLabel);
-            this.Controls.Add(this.passcodeTextBox);
-            this.Controls.Add(this.usernameTextBox);
-            this.Controls.Add(this.passcodeLabel);
-            this.Controls.Add(this.usernameLabel);
-            this.Controls.Add(this.scholarImageBox);
+            this.Controls.Add(this.gpRole);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.tbPasscode);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lblPasscode);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.imgScholar);
             this.Name = "LoginForm";
             this.Text = "LoginForm";
-            ((System.ComponentModel.ISupportInitialize)(this.scholarImageBox)).EndInit();
-            this.roleRadioButtonGroupBox.ResumeLayout(false);
-            this.roleRadioButtonGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgScholar)).EndInit();
+            this.gpRole.ResumeLayout(false);
+            this.gpRole.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,15 +167,15 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox scholarImageBox;
-        private System.Windows.Forms.Label usernameLabel;
-        private System.Windows.Forms.Label passcodeLabel;
-        private System.Windows.Forms.TextBox usernameTextBox;
-        private System.Windows.Forms.TextBox passcodeTextBox;
-        private System.Windows.Forms.Label errorLabel;
-        private System.Windows.Forms.Button loginButton;
-        private System.Windows.Forms.GroupBox roleRadioButtonGroupBox;
-        private System.Windows.Forms.RadioButton studentRadioButton;
-        private System.Windows.Forms.RadioButton teacherRadioButton;
+        private System.Windows.Forms.PictureBox imgScholar;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPasscode;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPasscode;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.GroupBox gpRole;
+        private System.Windows.Forms.RadioButton rdoStudent;
+        private System.Windows.Forms.RadioButton rdoTeacher;
     }
 }
