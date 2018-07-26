ebbef062aa6201bb3a92129e10f5030668bd4e3c
Aiden Madaffri <Aiden.Madaffri@gmail.com>
2018-07-26 11:06:42 -0500

    Added method to get letter grade, integrated into Student Gradebook

--- Neutrasoft Scholar/Assignment.cs
+++ Neutrasoft Scholar/Assignment.cs
@@ -19,6 +19,56 @@ namespace Neutrasoft_Scholar
         }
 
 
+
+        public static string GetLetterGrade(int grade)
+        {
+            if (grade >= 95)
+            {
+                return "A+";
+            }
+            else if (grade >= 90 && grade < 95)
+            {
+                return "A-";
+            }
+            else if (grade >= 85 && grade < 90)
+            {
+                return "B+";
+            }
+            else if (grade >= 80 && grade < 85)
+            {
+                return "B-";
+            }
+            else if (grade >= 75 && grade < 80)
+            {
+                return "C+";
+            }
+            else if (grade >= 70 && grade < 75)
+            {
+                return "C-";
+            }
+            else if (grade >= 65 && grade < 70)
+            {
+                return "D+";
+            }
+            else if (grade >= 60 && grade < 65)
+            {
+                return "D-";
+            }
+            else if (grade <= 59 && grade >= 0)
+            {
+                return "F";
+            }
+            else if (grade == -1)
+            {
+                return "No Grade";
+            }
+            else
+            {
+                throw new Exception("Number is negative");
+            }
+
+        }
+
         //Returns sorted list of assignments and grades
         public static Dictionary<string, List<Assignment>> GetStudentAssignments(Teacher teacher, Student student)
         {
